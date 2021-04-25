4ee413c639be5fcede9d09835abc6fa871b6b06d
BestLeaks <59940456+BestLeaks@users.noreply.github.com>
2021-04-25 02:06:04 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,14 @@
 config = {}
-CONFIG.DriveCars = false
-CONFIG.BlacklistWeaponsDefault = true
-CONFIG.RankSystemAllow = true
-
+CONFIG.DriveCars = false --Ability to drive/steal cars?
+CONFIG.BlacklistWeaponsDefault = true --Blacklist Weapons that we have set as default, Can be editied
+CONFIG.RankSystemAllow = true --Ranksystem Allow
+CONFIG.WeaponsOnBack = true --Makes players have there weapons on there back
 
 CONFIG.Keys =
 {
 
 }
 
+--false = Do not set
+--True = Do set
 
